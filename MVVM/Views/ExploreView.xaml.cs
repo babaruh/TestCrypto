260add69c26c6ea6a09d36657306d4a260983be7
babaruh <realmaksym@proton.me>
2023-03-16 10:09:31 +0200

    Fix: remove unused method from ExploreView.xaml.cs

--- MVVM/Views/ExploreView.xaml.cs
+++ MVVM/Views/ExploreView.xaml.cs
@@ -10,17 +10,4 @@ public partial class ExploreView : UserControl
 {
     public ExploreView() => 
         InitializeComponent();
-
-    private void TextBox_KeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key is not Key.Enter) return;
-        var textBox = sender as TextBox;
-
-        if(string.IsNullOrWhiteSpace(textBox?.Text)) return;
-        
-        var text = textBox.Text;
-
-        var viewModel = DataContext as ExploreViewModel;
-        viewModel?.NavigateCoinFullDataViewCommand.Execute(text);
-    }
 }
